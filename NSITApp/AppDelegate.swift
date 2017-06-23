e57db92be32c9c884ef0e6e51290edf7eda3565d
Manan Manwani <mananmanwani@manans-macbook-air.local>
2017-06-23 11:34:37 +0530

    Added Firebase Storage Support

--- NSITApp/AppDelegate.swift
+++ NSITApp/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
         FirebaseApp.configure()
         Database.database().isPersistenceEnabled = true
         return true
--- NSITApp/SyllabusViewController.swift
+++ NSITApp/SyllabusViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import  Firebase
 
 class SyllabusViewController: UIViewController,UINavigationBarDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var menu: UIBarButtonItem!
+    @IBOutlet weak var img: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,31 @@ class SyllabusViewController: UIViewController,UINavigationBarDelegate,UINavigat
         revealViewController().rearViewRevealWidth = 200
         menu.target = revealViewController()
         menu.action = #selector(SWRevealViewController.revealToggle(_:))
+        // Get a reference to the storage service using the default Firebase App
+        let storage = Storage.storage()
+        
+        // Create a storage reference from our storage service
+        let storageRef = storage.reference()
+        
+        // Note that you can use variables to create child values
+  //      let fileName = "graph.jpg"
+ //       let spaceRef = imagesRef.child(fileName)
+        // Create a reference to the file you want to download
+        let islandRef = storageRef.child("Graph.jpg")
+        
+        // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+        islandRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            if let error = error {
+                print (error)
+                // Uh-oh, an error occurred!
+            } else {
+                // Data for "images/island.jpg" is returned
+                let image = UIImage(data: data!)
+                self.img.image=image
+            }
+        }
+        
+
     }
 
     override func didReceiveMemoryWarning() {
